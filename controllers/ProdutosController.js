6801ea927d9d0c11304e6663da93e9c3e6799347
Leandr0-August0 <leco.muniz012@gmail.com>
2024-10-03 22:46:41 -0300

    td certo @Leandr0-August0

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -6,32 +6,57 @@ import Sequelize from "sequelize";
 
 // ROTA PRODUTOS
 router.get("/produtos", (req, res) => {
-    Categorias.findAll({
-        include: [
-            {
-                model: Produtos,
-                as: 'produtos',
-                required: false, // Isso faz um LEFT JOIN, mude para true para INNER JOIN
-            },
-        ],
-        // attributes: {
-        //     include: [
-        //         [
-        //             Sequelize.fn("COUNT", Sequelize.col("produtos.id")),
-        //             "produtosCount",
-        //         ],
-        //     ],
-        // },
-        // group: ["Categorias.id"],
-    })
-        .then((categorias) => {
-            res.render("produtos", {
-                categorias: categorias,
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+	Categorias.findAll({
+		include: {
+			model: Produtos,
+			as: "produtos",
+			required: false, // Isso faz um LEFT JOIN, mude para true para INNER JOIN
+		},
+	})
+		.then((categorias) => {
+			res.render("produtos", {
+				categorias: categorias,
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 });
+
+//ROTA DE CADASTRO
+router.post("/produto/new", (req, res) => {
+	const categoria = req.body.categoria;
+	const produto = req.body.produto;
+	const valor = req.body.valor;
+	const imagem = req.body.imagem;
+	Produtos.create({
+		produto: produto,
+		categoria: categoria,
+		valor: valor,
+		imagem: imagem,
+        avaliacao: null
+	})
+		.then(() => {
+			res.redirect("/produtos");
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
+
+router.post("/categoria/new", (req, res) => {
+	const categoria = req.body.nomeCat;
+	Categorias.create({
+		categoria: categoria,
+	})
+		.then(() => {
+			res.redirect("/produtos");
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
+
+//ROTA DE EDIÇÃO
 
 export default router;
