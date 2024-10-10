aeb3ee49145c8d2c86ad259129731b9bda2d20a3
Leandr0-August0 <leco.muniz012@gmail.com>
2024-10-09 22:26:15 -0300

    @Leandr0-August0

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -6,101 +6,106 @@ import Sequelize from "sequelize";
 
 // ROTA PRODUTOS
 router.get("/produtos", (req, res) => {
-    Categorias.findAll({
-        include: {
-            model: Produtos,
-            as: "produtos",
-            required: false, // Isso faz um LEFT JOIN, mude para true para INNER JOIN
-        },
-    })
-        .then((categorias) => {
-            res.render("produtos", {
-                categorias: categorias,
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+	Categorias.findAll({
+		include: {
+			model: Produtos,
+			as: "produtos",
+			required: false, // Isso faz um LEFT JOIN, mude para true para INNER JOIN
+		},
+	})
+		.then((categorias) => {
+			res.render("produtos", {
+				categorias: categorias,
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 });
 
 //ROTA DE CADASTRO
 router.post("/produto/new", (req, res) => {
-    const categoria = req.body.categoria;
-    const produto = req.body.produto;
-    const valor = req.body.valor;
-    const imagem = req.body.imagem;
-    Produtos.create({
-        produto: produto,
-        categoria: categoria,
-        valor: valor,
-        imagem: imagem,
-        avaliacao: null,
-    })
-        .then(() => {
-            res.redirect("/produtos");
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+	const categoria = req.body.categoria;
+	const produto = req.body.produto;
+	const valor = req.body.valor;
+	const imagem = req.body.imagem;
+	Produtos.create({
+		produto: produto,
+		categoria: categoria,
+		valor: valor,
+		imagem: imagem,
+		avaliacao: null,
+	})
+		.then(() => {
+			res.redirect("/produtos");
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 });
 
 router.post("/categoria/new", (req, res) => {
-    const categoria = req.body.nomeCat;
-    Categorias.create({
-        categoria: categoria,
-    })
-        .then(() => {
-            res.redirect("/produtos");
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+	const categoria = req.body.nomeCat;
+	Categorias.create({
+		categoria: categoria,
+	})
+		.then(() => {
+			res.redirect("/produtos");
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 });
 
 //ROTA DE EDIÇÃO
-router.get("/produto/edit/:id", (req, res) => {
-    const id = req.params.id;
-    Produtos.findByPk(id, {
-        include: {
-            model: Categorias,
-            as: "categoriaId",
-            attributes: ["categoria"],
-        },
-    }).then((prodEdit) => {
-        res.render("produtoEdit", {
-            produtos: prodEdit,
-        });
-    });
+router.get("/produto/edit/:id", async (req, res) => {
+	const id = req.params.id;
+	try {
+		const prodEdit = await Produtos.findByPk(id, {
+			include: {
+				model: Categorias,
+				as: "categoriaId",
+				attributes: ["categoria"],
+			},
+		});
+		const categorias = await Categorias.findAll();
+		res.render("produtoEdit", {
+			produtos: prodEdit,
+			categorias: categorias,
+		});
+	} catch {
+		console.log("Houve um erro na busca dos produtos");
+	} 
 });
 
 //ROTA DE ALTERAÇÃO
 router.post("/produto/update", (req, res) => {
-    // const id = req.body.id;
-    // const categoria = req.body.categoria;
-    // const produto = req.body.produto;
-    // const valor = req.body.valor;
-    // const imagem = req.body.imagem;
-    const { id, categoria, produto, valor, imagem } = req.body;
-    
-    Produtos.update(
-        {
-            categoria: categoria,
-            produto: produto,
-            valor: valor,
-            imagem: imagem,
-        },
-        {
-            where: {
-                id: id,
-            },
-        }
-    )
-        .then(() => {
-            res.redirect("/produtos");
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+	// const id = req.body.id;
+	// const categoria = req.body.categoria;
+	// const produto = req.body.produto;
+	// const valor = req.body.valor;
+	// const imagem = req.body.imagem;
+	const { id, categoria, produto, valor, imagem } = req.body;
+
+	Produtos.update(
+		{
+			categoria: categoria,
+			produto: produto,
+			valor: valor,
+			imagem: imagem,
+		},
+		{
+			where: {
+				id: id,
+			},
+		}
+	)
+		.then(() => {
+			res.redirect("/produtos");
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 });
 
 export default router;
