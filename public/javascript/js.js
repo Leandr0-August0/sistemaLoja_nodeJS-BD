721f94d6483b5c10c0a435cbd2fe55484ba1b19c
Leandr0-August0 <leco.muniz012@gmail.com>
2024-10-11 11:04:36 -0300

    @Leandr0-August0

--- public/javascript/js.js
+++ public/javascript/js.js
@@ -7,16 +7,3 @@ const year = date.getFullYear();
 const currentDate = document.querySelector(".currentDate");
 currentDate.innerHTML = `Novos pedidos foram realizados desde o último acesso ao sistema. Hoje é <strong>${day}/${month}/${year}</strong>.`;
 
-
-//CONFIRMAÇÃO DO DELETAR
-document
-    .getElementById("btnExcluir")
-    .addEventListener("click", function (event) {
-        var confirmar = confirm("Tem certeza que deseja enviar o formulário?");
-        if (confirmar) {
-            window.location.href(
-                `/clientes/delete/<%= cliente.id %>`
-            );
-        }
-            return;
-    });
